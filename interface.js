cb020df514fc8feee81b5fa7f1f494b5e0450a91
Carol Gardiner <student@Makerss-MacBook-Pro.local>
2019-04-14 23:10:57 +0100

    ADD inventory file

---
+++ interface.js
@@ -0,0 +1,7 @@
+const loadPage = () => {
+  let products = document.querySelector('#product-list')
+  let cart = document.querySelector('.cart-section')
+  console.log(products.parentNode)
+}
+
+document.onload = loadPage()
