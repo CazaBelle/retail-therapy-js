ae98df213c84be35ac7e89d2ee63ee9ac8e0782c
Carol Gardiner <student@Makerss-MacBook-Pro.local>
2019-04-15 11:35:27 +0100

    ADD feature of items being able to be added to the cart - tests passing

--- spec/CartSpec.js
+++ spec/CartSpec.js
@@ -1,11 +1,16 @@
 describe('Cart', () => {
-  
+  let item = [{ id: 1, name: "testshow", price: 50, quantity: 4}]
   it(`init with no product`, ()=>{
     let cart = new Cart()
     expect(cart.countCartItems()).toBe(0)
   })
 
-  
+  it('items can be added to the cart', ()=>{
+    let cart = new Cart()
+    
+    cart.addCartItem(1)
+    expect(cart.countCartItems()).toBe(1)
+  })
 
 })
 
--- src/Cart.js
+++ src/Cart.js
@@ -6,6 +6,16 @@ class Cart {
   countCartItems(){
     console.log(this.items.length)
     return this.items.length
+  }
+
+  addCartItem(id){
+      let cartItems = data.find(function(product){
+          if(product.id === id){
+              return product
+          }
+      })
+      cartItems.quantity = 1
+      this.items.push(cartItems)
   
   }
 }
